5eebba4cde90f79a4d34161ed66d1f73a503dcb2
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-08-14 22:15:01 +0600

    TypeScript Fundamentals

---
+++ 04. TypeScript/01. TypeScript Fundamentals/02_items.js
@@ -0,0 +1,7 @@
+"use strict";
+let items = 'laptop';
+//items = 100;      // error: Type 'number' is not assignable to type 'string'
+console.log(items);
+console.log("hello");
+console.log("World");
+console.log("Testing");
