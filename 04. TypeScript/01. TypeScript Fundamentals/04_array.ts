28ad2ed6d2ad2e0ea82420ce935eab854f28d7a0
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-08-15 19:42:52 +0600

    TypeScript Fundamentals

---
+++ 04. TypeScript/01. TypeScript Fundamentals/04_array.ts
@@ -0,0 +1,10 @@
+const numbers : number[] = [1, 2, 3, 4, 5];
+console.log(numbers);       // [1, 2, 3, 4, 5]
+
+// numbers.push('6'); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
+
+const myLocation: [number, number] = [23.456, 45]   
+console.log(myLocation);     // [ 23.456, 45 ]
+
+const entries: [string, number] = ['age', 24]
+console.log(entries);       // [ 'age', 24 ]
