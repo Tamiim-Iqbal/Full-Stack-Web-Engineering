271d695addebf11dd755b9bf5ba9ad0726cbbf64
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-08-15 19:41:58 +0600

    TypeScript Fundamentals

---
+++ 04. TypeScript/01. TypeScript Fundamentals/03_primitive_types.ts
@@ -0,0 +1,11 @@
+let district : string = "Dhaka";
+console.log(district);      // Dhaka
+
+let stock : number = 100;
+console.log(stock);         // 100
+
+let isAvailable : boolean = true;
+console.log(isAvailable);   // true
+
+let value : any;
+console.log(value);         // undefined
