b360b1f716cd613e790a9b2ada0ac0dd161c8ae3
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-08-15 19:43:49 +0600

    TypeScript Fundamentals

---
+++ 04. TypeScript/01. TypeScript Fundamentals/08_special_types.ts
@@ -0,0 +1,23 @@
+function calculateTax (amount: number | string, taxRate: number):number{
+    if(typeof amount === 'string'){
+        amount = parseInt(amount);
+    }
+    return amount * taxRate;
+}
+
+const myTax = calculateTax(100, 0.15);
+const yourTax = calculateTax('200', 0.2)
+
+// null
+const myFuture : number | null = null;
+
+// any
+let price: any = "hello";
+price = 100;
+
+
+// undefined 
+let money: number | undefined = undefined;
+
+// unknown & never
+
