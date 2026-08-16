258ebdc4139d349fde8434672a1d2d1a45835104
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-08-17 00:17:29 +0600

    More TypeScript

---
+++ 04. TypeScript/02. More TypeScript/2_array_of_object.js
@@ -0,0 +1,15 @@
+"use strict";
+const products = [
+    {
+        id: 1,
+        name: "Laptop",
+        price: 999,
+        description: "High-performance"
+    },
+    {
+        id: 2,
+        name: "Smart Phone",
+        price: 599,
+        description: "Latest Model"
+    }
+];
