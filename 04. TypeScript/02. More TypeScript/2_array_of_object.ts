42eb9ca1de24b035c04d83042da728a4adbbdb45
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-08-17 00:17:40 +0600

    More TypeScript

---
+++ 04. TypeScript/02. More TypeScript/2_array_of_object.ts
@@ -0,0 +1,21 @@
+type Product = {
+    id : number,
+    name : string,
+    price : number,
+    description : string
+}
+
+const products : Product[] = [
+    {
+        id : 1,
+        name : "Laptop",
+        price : 999,
+        description : "High-performance"
+    },
+    {
+        id : 2,
+        name : "Smart Phone",
+        price : 599,
+        description : "Latest Model"
+    }
+]
