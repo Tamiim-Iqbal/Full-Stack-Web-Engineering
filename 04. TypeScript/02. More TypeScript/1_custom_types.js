6915070a95111f5afd1216c80feaa2dd96517075
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-08-17 00:17:05 +0600

    More TypeScript

---
+++ 04. TypeScript/02. More TypeScript/1_custom_types.js
@@ -0,0 +1,12 @@
+"use strict";
+const robin = {
+    name: "Robin",
+    roll: 1,
+    major: "Computer Science",
+    gpa: 3.50
+};
+const mobin = {
+    name: "Robin",
+    roll: 1,
+    major: "Computer Science",
+};
