db9af9172929546081ca0594ecf41ec43fa14641
Tamiim-Iqbal <iqbal.tamim.0011@gmail.com>
2026-09-16 23:32:38 +0600

    Next.js

---
+++ 08. Next js/01-my-first-nextjs-app/src/app/docs/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DocsPage = () => {
+    return (
+        <div>
+            <h2>Our Official Documenatation</h2>
+        </div>
+    );
+};
+
+export default DocsPage;
---
+++ 08. Next js/01-my-first-nextjs-app/src/app/showcase/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ShowCasePage = () => {
+    return (
+        <div>
+            <h2>Our Showcases</h2>
+        </div>
+    );
+};
+
+export default ShowCasePage
